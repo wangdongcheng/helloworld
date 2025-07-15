afbe14aa06f1188b55cb35777874224069ffc106
wangdongcheng <wangdongcheng@gmail.com>
2025-07-15 16:05:38 +0200

    arraylist to list

--- Program.cs
+++ Program.cs
@@ -8,56 +8,18 @@ class Program
 {
     static void Main()
     {
-        string searchValue = "30AAV001"; // value to search for
-        string Database = "VJSCLTest"; // database name
+        string searchValue = "MAIN DIV"; // value to search for
+        string Database = "VJSCL"; // database name
         // Connection string to the SQL Server database
         string connectionString = $"Server=VJSERVER03;Database={Database};User Id=sa;Password=net1032;";
         string tableName = "";       // table name
-        ArrayList tablenames = new ArrayList();
+        var tablenames = new List<string>();
         // tablenames.Add("SL_ACC_TEMP");
-        // tablenames.Add("SL_ACCESS_PAYMENTS");
-        // tablenames.Add("SL_ACCESS_PAYMENTS_ERRORS");
-        tablenames.Add("SL_ACCOUNTS");
-        tablenames.Add("SL_ACCOUNTS2");
-        // tablenames.Add("SL_ACCOUNTS2_12012024");
-        tablenames.Add("SL_ACCOUNTS3");
-        tablenames.Add("SL_ADDRESSES");
-        tablenames.Add("SL_ADDRESSES2");
-        tablenames.Add("SL_ADDRESSES3");
-        // tablenames.Add("SL_ALLOC_HISTORY");
-        // tablenames.Add("SL_ALLOCATION_TEMP");
-        // tablenames.Add("SL_ANALYSIS");
-        // tablenames.Add("SL_ANALYSIS2");
-        // tablenames.Add("SL_AP_BANK_ANALYSES");
-        // tablenames.Add("SL_BACS_FORMATS");
-        // tablenames.Add("SL_BUYING_GROUP_MEMBERS");
-        // tablenames.Add("SL_BUYING_GROUPS");
-        // tablenames.Add("SL_CAMPAIGN_ASSIGN");
-        // tablenames.Add("SL_DD_SIGNUPS");
-        // tablenames.Add("SL_DD_SUGGESTED_COLLECTION_HISTORY");
-        // tablenames.Add("SL_DD_SUGGESTED_COLLECTION_TRANS");
-        // tablenames.Add("SL_DD_SUGGESTED_COLLECTIONS");
-        // tablenames.Add("SL_DETAIL_PLUGINS_DATETIME");
-        // tablenames.Add("SL_DETAIL_PLUGINS_VCHAR");
-        // tablenames.Add("SL_DIRECT_DEBIT_STATUSES");
-        // tablenames.Add("SL_LETTERS");
-        // tablenames.Add("SL_NOTES");
-        // tablenames.Add("SL_NOTES_LINK");
-        // tablenames.Add("SL_PL_ALLOC_CORRECT_TEMP");
-        // tablenames.Add("SL_PL_NL_DETAIL");
-        // tablenames.Add("SL_PL_NL_DETAIL_ACCRUALS");
-        // tablenames.Add("SL_PL_NL_DETAIL2");
-        // tablenames.Add("SL_PL_STATEMENTS_TEMP");
-        // tablenames.Add("SL_PROSPECT_LINK");
-        // tablenames.Add("SL_TERMS");
-        // tablenames.Add("SL_TRANSACTION_DD_COLLECTION_HISTORY");
-        // tablenames.Add("SL_TRANSACTION_NOTES");
-        // tablenames.Add("SL_TRANSACTIONS");
-        // tablenames.Add("SL_TRANSACTIONS2");
-        // tablenames.Add("SL_TRN_LOCKS");
-        // tablenames.Add("SL_TRN_TEMP");
+        tablenames.Add("STK_STOCK");
+        tablenames.Add("STK_STOCK_2");
+        tablenames.Add("STK_STOCK3");
+        tablenames.Add("STK_STOCK4");
 
-        
         var matchingColumns = FindMatchingColumns(connectionString, tableName, searchValue, tablenames);
 
         Console.WriteLine($"the columns that contain the value {searchValue} in table {tableName}:");
@@ -67,7 +29,7 @@ class Program
         }
     }
 
-    static List<string> FindMatchingColumns(string connStr, string tableName, string searchValue, ArrayList tablenames)
+    static List<string> FindMatchingColumns(string connStr, string tableName, string searchValue, List<string> tablenames)
     {
         var matchingColumns = new List<string>();
         using (var conn = new SqlConnection(connStr))
