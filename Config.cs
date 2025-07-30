fbc4bfe04eb523ebc16d65bfb38eb379910d4e3c
wangdongcheng <wangdongcheng@gmail.com>
2025-07-30 16:14:55 +0200

    1

--- Config.cs
+++
@@ -1,6 +0,0 @@
-class Config
-{
-    // the object according to the config.json file to hold database configuration
-    public Database? Database { get; set; }
-    public string[]? Tablenames { get; set; }
-}
